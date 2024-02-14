73fa419ae7b8d740730738c2a18e578f4546ee8f
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-02-14 12:27:48 +0300

    Add project files.

---
+++ APITest/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace APITest.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Gym { get; set; }
+    }
+}
---
+++ APITest/Models/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace APITest.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
