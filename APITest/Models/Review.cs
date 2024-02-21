48fd934e5420e0131b0c45d344f34191f4f9965a
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-02-21 23:07:01 +0300

    Update relations

--- APITest/Models/Review.cs
+++ APITest/Models/Review.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
         public string Text { get; set; }
+        public Review Reviewer { get; set; }
+        public Pokemon Pokemonoke { get; set; }
     }
 
 }
