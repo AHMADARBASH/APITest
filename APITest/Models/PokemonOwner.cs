7ed2c1a775d032bea39dc73bfd53d217d9b532c4
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-02-22 14:37:39 +0300

    Create DBContext

--- APITest/Models/PokemonOwner.cs
+++ APITest/Models/PokemonOwner.cs
@@ -5,7 +5,7 @@
         public int PokemonId { get; set; }
         public int OwnerId {  get; set; }  
         public Pokemon Pokemon { get; set; }
-        public Owner Onwer { get; set; }
+        public Owner Owner { get; set; }
 
     }
 }
--- APITest/Models/Review.cs
+++ APITest/Models/Review.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public Review Reviewer { get; set; }
+        public Reviewer Reviewer { get; set; }
         public Pokemon Pokemonoke { get; set; }
     }
 
